113fff7510deb4b15bf58088a230d3029e55293c
Fabienne Rondi <f.rondi@orange.fr>
2021-04-12 19:20:23 +0200

    Création fichier test pour méthodes de Tournament.py

---
+++ tests/test_tournament.py
@@ -0,0 +1,31 @@
+import unittest
+
+from application.models.tournament import Tournament
+
+
+class TestTournament(unittest.TestCase):
+    """[summary]
+
+    Arguments:
+        unittest {[type]} -- [description]
+    """
+
+    def setUp(self):
+        """Create an instance of Player
+        """
+        self.player = None
+
+
+    def test_tournament_players(self):
+        """Set a full name for this player
+        """
+        pass
+
+    def test_tournament_rounds(self):
+        """update ranking's player
+        """
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
