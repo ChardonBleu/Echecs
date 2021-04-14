87cc0b125834a65fb53c3309ff0f0a14d495717f
Fabienne Rondi <f.rondi@orange.fr>
2021-04-14 14:03:22 +0200

    Test des méthodes ajoutées pour la gestion du premier round

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,6 +1,10 @@
 import unittest
 
 from application.models.player import Player
+from application.models.playermanager import PlayerManager
+from application.controllers.playercontroller import PlayerController
+from application .views.playerview import PlayerView
+
 
 
 class TestPlayer(unittest.TestCase):
@@ -13,12 +17,24 @@ class TestPlayer(unittest.TestCase):
     def setUp(self):
         """Create an instance of Player
         """
-        self.player = Player("Judit", "Polgar", "1976", "F", "2735")
+        self.player = Player("Judit", "Polgar", "1976", "F", 2735)
         self.serialized_player = {'first_name': 'Judit',
                                   'last_name': 'Polgar',
                                   'birth_date': '1976',
                                   'sexe': 'F',
-                                  'ranking': '2735'}
+                                  'ranking': 2735}
+        self.player_manager = PlayerManager()
+        self.player_manager.players = [Player("bérénice", "", "", "", 451),
+                                       Player("zoé", "",  "", "", 120),
+                                       Player("hector", "", "", "", 630),
+                                       Player("anatole", "", "", "", 256),
+                                       Player("jules", "", "", "", 1200),
+                                       Player("doris", "", "", "", 820),
+                                       Player("garance", "", "", "", 567),
+                                       Player("wallace", "", "", "", 973)]
+        [4, 1, 6, 7, 3, 5, 8, 2]
+        self.player_manager.indice = [1, 2, 3, 4, 5, 6, 7, 8]
+        self.player_controller = PlayerController()
 
     def test_full_name(self):
         """Set a full name for this player
@@ -41,6 +57,38 @@ class TestPlayer(unittest.TestCase):
         """Deserialize_player
         """
         self.assertEqual(str(self.player.deserialize_player(self.serialized_player)), str(self.player))
+        
+    def test_sort_players_by_ranking(self):
+        """sort players of PlayerManager object by ranking
+        """
+        sorted_player_manager = PlayerManager()
+        sorted_player_manager.players = [Player("jules", "", "", "", 1200),
+                                 Player("wallace", "", "", "", 973),
+                                 Player("doris", "", "", "", 820),
+                                 Player("hector", "", "", "", 630),
+                                 Player("garance", "", "", "", 567),
+                                 Player("bérénice", "", "", "", 451),
+                                 Player("anatole", "", "", "", 256),
+                                 Player("zoé", "", "", "", 120)]
+        sorted_player_manager.indice = [5, 8, 6, 3, 7, 1, 4, 2]
+        self.player_controller.sort_players_by_ranking(self.player_manager)
+        self.assertEqual(str(self.player_manager), str(sorted_player_manager))
+        
+    def test_sort_players_by_name(self):
+        """sort players of PlayerManager object by ranking
+        """
+        sorted_player_manager = PlayerManager()
+        sorted_player_manager.players = [Player("anatole", "", "", "", 256),
+                                         Player("bérénice", "", "", "", 451),
+                                         Player("doris", "", "", "", 820),
+                                         Player("garance", "", "", "", 567),
+                                         Player("hector", "", "", "", 630),
+                                         Player("jules", "", "", "", 1200),
+                                         Player("wallace", "", "", "", 973),
+                                         Player("zoé", "", "", "", 120)]
+        sorted_player_manager.indice = [4, 1, 6, 7, 3, 5, 8, 2]
+        self.player_controller.sort_players_by_name(self.player_manager)
+        self.assertEqual(str(self.player_manager), str(sorted_player_manager))
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_tournament.py
+++ tests/test_tournament.py
@@ -2,6 +2,8 @@ import unittest
 
 from application.models.tournament import Tournament
 from application.models.round import Round
+from application.models.round import Match
+from application.models.player import Player
 
 
 class TestTournament(unittest.TestCase):
@@ -19,7 +21,11 @@ class TestTournament(unittest.TestCase):
                                  "Aujourd'hui",
                                  "demain",
                                  "Quel beau tournoi!", 2, 4)
-    
+        self.tournament.rounds = []
+        self.player1 = Player("joueur1", "", "", "", 2000)
+        self.player2 = Player("joueur2", "", "", "", 2100)
+        self.match = Match(self.player1, self.player2, 0,0)
+
     def test_tournament_players(self):
         """teste que la méthode met bien dans l'attribut self.players de Tournament
         la liste passée en argument
@@ -28,6 +34,26 @@ class TestTournament(unittest.TestCase):
         self.tournament.tournament_players(liste_id_players)
         self.assertListEqual(self.tournament.players, [9, 10, 11, 12, 13, 14, 15, 16])
 
+    def test_add_round(self):
+        """[summary]
+        """
+        initial_number_of_rounds = len(self.tournament.rounds)
+        self.tournament.add_round()
+        self.assertEqual(len(self.tournament.rounds), initial_number_of_rounds + 1)
+
+    def test_add_match_to_last_round(self):
+        """[summary]
+        """
+        self.tournament.add_round()
+        initial_number_of_matches = len(self.tournament.rounds[0].matches)
+        self.tournament.add_match_to_last_round(self.player1, self.player2, 0, 0)
+        self.assertEqual(len(self.tournament.rounds[0].matches), initial_number_of_matches + 1)
+
+    def test_update_scores(self):
+        """[summary]
+        """
+        self.match.update_score(1,3)
+        self.assertEqual(str(self.match), str(Match(self.player1, self.player2, 1,3)))
 
 
 if __name__ == "__main__":
