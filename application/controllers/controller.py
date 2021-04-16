61871658989875b8b8abbca5d1cfb9edcb02df59
Fabienne Rondi <f.rondi@orange.fr>
2021-04-16 13:37:08 +0200

    Relecture méthodes gestion rounds et matchs dans Controller

--- application/controllers/controller.py
+++ application/controllers/controller.py
@@ -16,20 +16,19 @@ class Controller:
         self.round_controller = RoundController()
 
     def link_players_with_tournament(self):
-        """Associe la liste des joueurs chargés au tournoi courant
-        en renseignant l'attribut self.players de la classe Tournament
-        avec la liste des id des joueurs.
-        Cet id est celui du joueur dans la BDD
+        """Prend la liste des joueurs chargés depuis la bdd ou bien saisie à la main 
+        et l'associe au tournoi courant en renseignant l'attribut self.players de la classe Tournament.
+        Cette liste contient les id des joueurs dans la table players de la bdd.
         """
         id_list = self.players_controller.players_manager.liste_id_players
         self.tournament_controller.tournament.tournament_players(id_list)
 
     def start_round_with_control(self, nb_rounds):
-        """Créée un nouveau round en veillant à ce que les joueurs ne se soient pas déjà affrontés
+        """Crée un nouveau round en veillant à ce que les joueurs ne se soient pas déjà affrontés.
         Méthode appelée aprés nouveau tri des joueurs par classement et score.
 
         Arguments:
-            nb_rounds (int) -- transporté de méthode en méthode pour évaluer le nombre de match déjà créés
+            nb_rounds (int) -- transporté de méthode en méthode pour évaluer le nombre de matchs déjà créés.
         """
         self.tournament_controller.tournament.add_round()
         liste_index_joueur = [0, 1, 2, 3, 4, 5, 6, 7]  # Permet de décompter les joueurs disponibles pour appairage
@@ -38,83 +37,84 @@ class Controller:
 
     def search_couple_for_first_player(self, liste_index_joueur, nb_rounds):
         """Lance la méthode de recherche d'un joueur à appairer avec le premier joueur de la liste.
-        Un fois ce joueur trouvé supprime de la liste des index des joueurs ces deux joueurs
+        Une fois ce joueur trouvé on supprime de la liste les index de ces deux joueurs.
 
         Arguments:
-            liste_index_joueur {list} -- liste des index des joueurs disponibles pour appairage
-            nb_rounds (int) -- transporté de méthode en méthode pour évaluer le nombre de match déjà créés
+            liste_index_joueur (list) -- liste des index des joueurs disponibles pour appairage
+            nb_rounds          (int)  -- transporté de méthode en méthode pour évaluer le nombre de match déjà créés
 
         Returns:
             liste_index_joueur {list} -- liste modifiée des index des joueurs disponibles pour appairage
         """
         player_to_pair = liste_index_joueur[0]
         number_matches_before = self.tournament_controller.tournament.rounds[nb_rounds - 1].len_matches_list
-        saut = self.examine_other_players_as_candidate(liste_index_joueur, player_to_pair, 1)
-        other_player = liste_index_joueur[saut]
+        # On lance la méthode de recherche d'appairage et on récupère l'index du joueur sélectionné 
+        index_other_player = self.examine_other_players_as_candidate(liste_index_joueur, player_to_pair)
+        other_player = liste_index_joueur[index_other_player]
         # si le saut mène au dernier joueur de la liste et qu'aucun match n'a été encore ajouté, on le rajoute
         number_matches_after = self.tournament_controller.tournament.rounds[nb_rounds - 1].len_matches_list
-        if saut == len(liste_index_joueur) - 1 and number_matches_before == number_matches_after:
-            self.add_match_with_control(player_to_pair, other_player)
+        if index_other_player == len(liste_index_joueur) - 1 and number_matches_before == number_matches_after:
+            self.add_match_and_memorise(player_to_pair, other_player)
         # On retire les joueurs mis en match de la liste des joueurs dispo pour appairage:
         liste_index_joueur.remove(player_to_pair)
         liste_index_joueur.remove(other_player)
         return liste_index_joueur
 
-    def examine_other_players_as_candidate(self, liste_index_joueur, player_to_pair, saut):
+    def examine_other_players_as_candidate(self, liste_index_joueur, player_to_pair, index_other_player=1):
         """Recherche un joueur à appairer avec le joueur d'index player_to_pair
 
         Arguments:
-            liste_index_joueur {list} -- liste des index des joueurs disponibles pour appairage
-            player_to_pair {int} -- valeur du joueur à appairer dans liste_index_joueur
-                                    (valeur de liste_index_joueur[0])
-            saut {int} -- correspond à index de other_player dans liste_index_joueur
+            liste_index_joueur (list) -- liste des index des joueurs disponibles pour appairage
+            player_to_pair     (int)  -- valeur du joueur à appairer dans liste_index_joueur
+                                         (valeur de liste_index_joueur[0])
+            index_other_player (int)  --  correspond à index de other_player dans liste_index_joueur
 
         Returns:
-            saut (int) -- correspond à index de other_player dans liste_index_joueur
+            index_other_player (int) -- correspond à index de other_player dans liste_index_joueur
         """
-        other_player = liste_index_joueur[saut]
-        couple = (self.players_controller.players_manager.bdd_id[player_to_pair],
-                  self.players_controller.players_manager.bdd_id[other_player])
-        if couple not in self.round_controller.memo_match:
-            self.add_match_with_control(player_to_pair, other_player)
+        other_player = liste_index_joueur[index_other_player]
+        candidate_couple = (self.players_controller.players_manager.bdd_id[player_to_pair],
+                            self.players_controller.players_manager.bdd_id[other_player])
+        if candidate_couple not in self.round_controller.memo_match:
+            self.add_match_and_memorise(player_to_pair, other_player)
         else:
-            if saut < len(liste_index_joueur) - 1:
-                saut = self.examine_other_players_as_candidate(liste_index_joueur, player_to_pair, saut + 1)
-        return saut
+            if index_other_player < len(liste_index_joueur) - 1:
+                index_other_player = self.examine_other_players_as_candidate(liste_index_joueur, player_to_pair, index_other_player + 1)
+        return index_other_player
 
-    def add_match_with_control(self, index_joueur, other_player):
-        """Rajoute au tournoi un match entre les joueurs d'index index_joueur et other_player
-        dans l'attribut self.bdd_id de PlayerManager
+    def add_match_and_memorise(self, player_to_pair, other_player):
+        """Rajoute au tournoi un match entre les joueurs d'index "player_to_pair" et "other_player"
 
         Arguments:
-            index_joueur {[type]} -- [description]
-            saut {[type]} -- [description]
+            player_to_pair (int) -- index permettant de récupérer l'id du joueur
+            other_player (int) -- index permettant de récupérer l'id du joueur
         """
         self.tournament_controller.tournament.add_match_to_last_round(
-            self.players_controller.players_manager.bdd_id[index_joueur],
+            self.players_controller.players_manager.bdd_id[player_to_pair],
             self.players_controller.players_manager.bdd_id[other_player], 0, 0)
-        self.memorise_match_historical(index_joueur, other_player)
+        self.memorise_matches(player_to_pair, other_player)
 
-    def memorise_match_historical(self, index_joueur, other_player):
+    def memorise_matches(self, player_to_pair, other_player):
         """Stocke dans une liste de tupples dans l'attribut self.memo_match de RoundController
         les couples de joueurs s'étant déjà affrontés: (joueur1, joueur2) et (joueur2, joueur1)
 
-        Args:
-            index_joueur {int} -- index du joueuer dans la l'attribut self.bdd_id de PlayerManager
+        Arguments:
+            player_to_pair (int) -- index permettant de récupérer l'id du joueur
+            other_player (int) -- index permettant de récupérer l'id du joueur
         """
         self.round_controller.memo_match.append(
-            (self.players_controller.players_manager.bdd_id[index_joueur],
+            (self.players_controller.players_manager.bdd_id[player_to_pair],
              self.players_controller.players_manager.bdd_id[other_player]))
         self.round_controller.memo_match.append(
             (self.players_controller.players_manager.bdd_id[other_player],
-             self.players_controller.players_manager.bdd_id[index_joueur]))
+             self.players_controller.players_manager.bdd_id[player_to_pair]))
 
     def update_scores_players(self, results_round):
         """Récupère le dico des {id_player: score} pour mettre à jour les scores
         des joueurs dans Player
 
         Args:
-            results_round {dict} -- dico des {id_player: score}
+            results_round (dict) -- dico des {id_player: score}
         """
         for id_players, round_score in results_round.items():
             player = self.players_controller.players_manager[id_players]
